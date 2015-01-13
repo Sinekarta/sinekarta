9a489a7a4af1d6343f32f924c1073539081c31ad
deprato.alessandro@yahoo.it <deprato.alessandro@yahoo.it@4bdcd56d-cca0-e003-7381-dfb8ef12df11>
2015-01-13 19:33:38 +0000

    migliorata la gestione degli errori lato alfresco

--- trunk/sinekarta-ds/sinekarta-ds-alfresco/src/main/java/org/sinekartads/alfresco/webscripts/keyring/SkdsKeyStoreOpenWS.java
+++ trunk/sinekarta-ds/sinekarta-ds-alfresco/src/main/java/org/sinekartads/alfresco/webscripts/keyring/SkdsKeyStoreOpenWS.java
@@ -31,60 +31,68 @@ public class SkdsKeyStoreOpenWS
 			Status status, 
 			Cache cache ) {
 
+		InputStream is = null;
 		SkdsKeyStoreOpenResponse resp = new SkdsKeyStoreOpenResponse();
+		
+
+		String keyStoreName = "sinekarta.jks";
+		byte[] keyStoreBytes = null;
+		NodeRef keyStoreRef = null;
 		try {
-			String keyStorePin = req.getKeyStorePin();
-			char[] ksPwd = null;
-			if ( StringUtils.isNotBlank(keyStorePin) ) {
-				ksPwd = keyStorePin.toCharArray();
-			}
-			
-			String keyStoreName = "sinekarta.jks"; 
 			String userName = authenticationService.getCurrentUserName ( );
 			NodeRef personNodeRef = personService.getPerson ( userName );
 			NodeRef homespaceNodeRef = (NodeRef) nodeService.getProperty ( 
 					personNodeRef, ContentModel.PROP_HOMEFOLDER );
-			
-			NodeRef keyStoreRef = nodeService.getChildByName ( 
-					homespaceNodeRef, ContentModel.ASSOC_CONTAINS, keyStoreName );
-			
+			keyStoreRef = nodeService.getChildByName ( homespaceNodeRef, ContentModel.ASSOC_CONTAINS, keyStoreName );
 			ContentReader reader = contentService.getReader(keyStoreRef, ContentModel.PROP_CONTENT);
-			InputStream is = reader.getContentInputStream ( );
-			byte[] keyStoreBytes = IOUtils.toByteArray ( is );
+			is = reader.getContentInputStream ( );
+			keyStoreBytes = IOUtils.toByteArray ( is );
+		} catch(Exception e) {
+			keyStoreBytes = null;
+			processError(resp, e, "skds.error.missingKeyStore");
+		} finally {
 			IOUtils.closeQuietly(is);
-			
-			
-			KeyStoreType type = null;
-			KeyStoreType[] types = KeyStoreType.values(); 
-			KeyStore keyStore = null;
-			for ( int i=0; i<types.length && keyStore == null; i++ ) {
-				is = new ByteArrayInputStream ( keyStoreBytes );
-				type = types[i];
-				try {
+		}
+
+		String keyStorePin = req.getKeyStorePin();
+		KeyStore keyStore = null;
+		KeyStoreType type = null;
+		if ( keyStoreBytes != null ) {
+			try {
+				char[] ksPwd = null;
+				if ( StringUtils.isNotBlank(keyStorePin) ) {
+					ksPwd = keyStorePin.toCharArray();
+				}
+				KeyStoreType[] types = KeyStoreType.values(); 
+				for ( int i=0; i<types.length && keyStore == null; i++ ) {
+					is = new ByteArrayInputStream ( keyStoreBytes );
+					type = types[i];
 					keyStore = KeyStore.getInstance ( type.getType(), type.getProvider() );
 					keyStore.load ( is, ksPwd );
-				} catch (Exception e) {
-					tracer.info(String.format("%s - %s", type, e.getMessage()), e);
-					keyStore = null;
 				}
+			} catch(Exception e) {
+				keyStore = null;
+				processError(resp, e, "skds.error.keyStoreUnavailable");
 			}
+		}
 			
-			KeyStoreDTO ksDto = new KeyStoreDTO();
-			if ( keyStore != null ) {
-				ksDto.setName(keyStoreName);
-				ksDto.setPin(keyStorePin);
-				ksDto.setSupport(KeyRingSupport.ALFRESCO.name());
-				ksDto.setType(type.getType());
-				ksDto.setProvider(type.getProvider());
-				ksDto.setAliases ( TemplateUtils.Conversion.enumerationToArray(keyStore.aliases()) );
-				ksDto.setReference(keyStoreRef.toString());
-				resp.setKeyStore(ksDto);
-			} 
-			resp.resultCodeToString(ResultCode.SUCCESS);
-		} catch(Exception e) {
-			Logger.getLogger(getClass()).error(e.getMessage(), e);
-			resp.resultCodeToString(ResultCode.INTERNAL_SERVER_ERROR);
-		}	
+		if ( keyStore != null ) {
+			try {
+				KeyStoreDTO ksDto = new KeyStoreDTO();
+				if ( keyStore != null ) {
+					ksDto.setName(keyStoreName);
+					ksDto.setPin(keyStorePin);
+					ksDto.setSupport(KeyRingSupport.ALFRESCO.name());
+					ksDto.setType(type.getType());
+					ksDto.setProvider(type.getProvider());
+					ksDto.setAliases ( TemplateUtils.Conversion.enumerationToArray(keyStore.aliases()) );
+					ksDto.setReference(keyStoreRef.toString());
+					resp.setKeyStore(ksDto);
+				} 
+			} catch(Exception e) {
+				processError(resp, e, "skds.error.internalServerError");
+			}
+		}
 		return resp;
 	}
 }
